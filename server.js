cd031e64eaeb97f8f9e9a92f1748cf05176e499b
Daniel Lucio <Daniel@Daniels-MacBook-Air-2.local>
2017-11-01 23:20:54 -0500

    first draft, working examples

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var app = express();
 var port = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // Intial friend data 
@@ -28,12 +28,12 @@ var friends = [
   },
   {
     name: "Frodo Baggins",
-    number: "https://seekinggoddailyblog.files.wordpress.com/2016/07/1b9384b6de87ab45a1391d454bd695c5.jpg",
+    photo: "https://seekinggoddailyblog.files.wordpress.com/2016/07/1b9384b6de87ab45a1391d454bd695c5.jpg",
     scores: [3,3,3,3,3,3,3,3,3,3]
   },
   {
     name: "Legolas",
-    number: "https://i.pinimg.com/736x/04/80/29/048029f362c484a2a46b928afbe98837--orlando-bloom-legolas-eye-candy.jpg",
+    photo: "https://i.pinimg.com/736x/04/80/29/048029f362c484a2a46b928afbe98837--orlando-bloom-legolas-eye-candy.jpg",
     scores: [4,4,4,4,4,4,4,4,4,4]
   }
 ];
@@ -71,20 +71,16 @@ app.post("/api/friends", function(req, res) {
   var matchValue = 0; 
 
   // Cycles through all friends
-  for (i = 0; i <= friends.length; i++){
+  for (i = 0; i < friends.length; i++){
 
     // Cycles through scores of the user and the friend 
-    for (j = 0; j <= 10; j++) {
-
-      // Debugging 
-      console.log(matchValue);
-      console.log(newFriend.scores[j]);
+    for (j = 0; j < 10; j++) {
 
       // Finds the difference between the user score and friend score, returns absolute value, and adds that value to matchValue
       matchValue = matchValue + (Math.abs(newFriend.scores[j] - friends[i].scores[j]))
 
       //logs final match value to console 
-      console.log(friend[i].name + " " + matchValue);
+      console.log(friends[i].name + " " + matchValue);
 
       // Checks to see if the current friend's match value is less than the friend who is in there
       if (matchValue < match[0]){
@@ -92,10 +88,11 @@ app.post("/api/friends", function(req, res) {
         match[1] = i;  //set the best match to the current friend 
       }
 
+    } // Ends the for loop that cycles through scores 
+
       // Resets the match value 
       matchValue = 0;
 
-    } // Ends the for loop that cycles through scores 
   } // Ends the for loop that cycles through friends 
 
   var bestMatch = match[1];
